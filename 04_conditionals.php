8856fe3bf9d3c4ad6f88dd4e3790adff7058c2c1
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-05 15:53:29 -0400

    php conditionals

---
+++ 04_conditionals.php
@@ -0,0 +1,35 @@
+<?php
+
+// $age = 20;
+// if ($age >= 21) {
+//   echo "You may drink";
+// } else {
+//   echo "Sorry, you arn't allowed to drink";
+// };
+
+// $time = date("F j"); // Just google when need to use.
+// echo $time;
+
+// $post = ["Post"];
+// if(!empty($post)) {
+//   echo 123;
+// };
+
+// $firstPost =  !empty($post) ? $post[0] : "No post";
+// echo $firstPost;
+
+// $firstPost = $post[0] ?? null;
+// echo $firstPost;
+
+$facColor = "yellow";
+switch($facColor) {
+case "red" : echo "It's red";
+break;
+case "blue" : echo "It's blue";
+break;
+case "green" : echo "It's green";
+break;
+default: echo "That is not my favorite color";
+}
+
+?>
